f7b4fbb47d235c5ecce31c898868907d11077d9d
Kyle Long <kjlong@cpp.edu>
2022-10-05 19:26:51 -0700

    Adding initial outline comments

---
+++ ticTacToe.c
@@ -0,0 +1,20 @@
+//Tic Tac Toe game in between 2 players or player vs computer
+
+
+int main() {
+
+    //prompt user for game they wish to play
+    
+    //read information from console
+
+    //print current status of the board
+
+    //prompt player/computer to make their move
+
+    //print out new status
+
+    //repeat
+
+
+    return 0;
+}
