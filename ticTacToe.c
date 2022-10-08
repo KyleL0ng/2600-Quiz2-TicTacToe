84995ce8f452f46e4c42a90e62e6f68d78f4cd17
Kyle Long <kjlong@cpp.edu>
2022-10-07 19:35:39 -0700

    Working move code, duplicates still allowed

--- ticTacToe.c
+++ ticTacToe.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 
-//Tic Tac Toe game in between 2 players or player vs computer
+// Tic Tac Toe game in between 2 players or player vs computer
 
 int intro();
 void printBoard(char state[]);
-void makeTurn(int turns);
 char determineXY(int turns);
 
-int main() {
+int main()
+{
 
-    char state [9];
+    char state[9];
+    state[0] = ' ';
     state[1] = ' ';
     state[2] = ' ';
     state[3] = ' ';
@@ -20,65 +21,123 @@ int main() {
     state[6] = ' ';
     state[7] = ' ';
     state[8] = ' ';
-    state[9] = ' ';
     int turns = 0;
     int x;
     int y;
 
-    int choice = intro(); //Prompts user and stores game choice
-    //NEED to make choice do something
+    int choice = intro(); // Prompts user and stores game choice
+    // NEED to make choice do something
 
-    //print current status of the board
+    // print current status of the board
     printBoard(state);
 
-    while (turns < 9) {
-        
-        makeTurn(turns);
+    while (turns < 9)
+    {
+        int A = 0; // tracks row
+        int B = 0; // tracks column
+        int player = 0;
+        char result[2];
 
-        switch(x, y) {
-        case '1, 1' :
-            state[1] = determineXY(turns);
-            break;
-        case '1, 2' :
-            state[2] = determineXY(turns);
-            break;
-        case '1, 3' :
-            state[3] = determineXY(turns);
-            break;
-        case '2, 1' :
-            state[4] = determineXY(turns);
-            break;
-        case '2, 2' :
-            state[5] = determineXY(turns);
-            break;
-        case '2, 3' :
-            state[6] = determineXY(turns);
-            break;
-        case '3, 1' :
-            state[7] = determineXY(turns);
-            break;
-        case '3, 2' :
-            state[8] = determineXY(turns);
-            break;
-        case '3, 3' :
-            state[9] = determineXY(turns);
-            break;
+        if (determineXY(turns) == 'X')
+        {
+            player = 1;
+        }
+        else
+        {
+            player = 2;
+        }
+        printf("Player%d: make your move", player);
+        scanf("%d %d", &A, &B);
+        if (A > 3)
+        {
+            printf("invalid move");
+            scanf("%d %d", &A, &B);
+        }
+        if (A < 1)
+        {
+            printf("invalid move");
+            scanf("%d %d", &A, &B);
+        }
+        if (B > 3)
+        {
+            printf("invalid move");
+            scanf("%d %d", &A, &B);
+        }
+        if (B < 1)
+        {
+            printf("invalid move");
+            scanf("%d %d", &A, &B);
+        }
+
+        if (A == 1)
+        {
+            if (B == 1)
+            {
+                // if (state[0] != ' ')
+                // {
+                //     printf("invalid move");
+                //     scanf("%d %d", &A, &B);
+                // }
+                // else
+                // {
+                    state[0] = determineXY(turns);
+               // }
+            }
+            else if (B == 2)
+            {
+                state[1] = determineXY(turns);
+            }
+            else if (B == 3)
+            {
+                state[2] = determineXY(turns);
+            }
+        }
+        else if (A == 2)
+        {
+            if (B == 1)
+            {
+                state[3] = determineXY(turns);
+            }
+            else if (B == 2)
+            {
+                state[4] = determineXY(turns);
+            }
+            else if (B == 3)
+            {
+                state[5] = determineXY(turns);
+            }
+        }
+        else if (A == 3)
+        {
+            if (B == 1)
+            {
+                state[6] = determineXY(turns);
+            }
+            else if (B == 2)
+            {
+                state[7] = determineXY(turns);
+            }
+            else if (B == 3)
+            {
+                state[8] = determineXY(turns);
+            }
+        }
+        printBoard(state);
+        turns++;
     }
-    turns++;
-    }
-    
-    //prompt player/computer to make their move
 
-    //print out new status
+    // prompt player/computer to make their move
 
-    //repeat
+    // print out new status
 
+    // repeat
 
     return 0;
 }
 
-int intro () {
-    int choice; //tracks whether user wants to play vs computer or other player
+int intro()
+{
+    int choice; // tracks whether user wants to play vs computer or other player
     printf("===========================\n");
     printf("WELCOME TO TIC TAC TOE!\n");
     printf("1 --- person vs. person\n");
@@ -89,33 +148,22 @@ int intro () {
     return choice;
 }
 
-void printBoard (char state[]) {
-    printf("+-------------+\n | %c | %c | %c |\n+-------------+\n | %c | %c | %c |\n+-------------+\n | %c | %c | %c |\n+-------------+\n", state[1], state[2], state[3], state[4], state[5], state[6], state[7], state[8], state[9]);
+void printBoard(char state[])
+{
+    printf("+-------------+\n | %c | %c | %c |\n+-------------+\n | %c | %c | %c |\n+-------------+\n | %c | %c | %c |\n+-------------+\n", state[0], state[1], state[2], state[3], state[4], state[5], state[6], state[7], state[8]);
 }
-char determineXY (int turns) {
-     char player; //tracks whether to add an X or Y
-     
-     if (turns%2 == 0) {
+char determineXY(int turns)
+{
+    char player; // tracks whether to add an X or Y
+
+    if (turns % 2 == 0)
+    {
         player = 'X';
-     }
-     else {
+    }
+    else
+    {
         player = 'Y';
-     }
+    }
 
     return player;
-}
-
-void makeTurn(int turns) {
-    int A = 0; //tracks row
-    int B = 0; //tracks column
-    int player = 0;
-
-    if (determineXY(turns) == 'X') {
-        player = 1;
-    }
-    else {
-        player = 2;
-    }
-    printf("Player%d: make your move");
-    scanf("%d %d", &A, &B);
 }
